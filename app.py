8bcb1cf7776c4aa235e88d21fc419647ab0795b7
Emersonp7 <emersonp935@gmail.com>
2024-11-02 14:21:55 -0400

    Game Class

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 from flask import Flask, render_template, url_for
 
+class Game:
+    def __init__(self, day) -> None:
+        self.day = day
+        self.expand = False
+        self.citizens = 0
+        self.standing = 0
+
+        # Map Zones
+        self.zones = 9
+        self.residentZones = 0
+        self.commercialZones = 0
+        self.industrialZones = 0
+        self.militaryZones = 0
+
+        # currency
+        self.gears = 100
+        # resources
+        self.steam = 1 # One steam for 10 citizens
+        
+    def endDay(self):
+        self.day -= 1
+        self.gears -= self.militaryZones * 10
+        self.updateResources()
+    
+    def updateResources(self):
+        return None
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+
     return render_template('index.html')
 
+@app.route('/game')
+def game():
+    return render_template("game.html")
 
 
 if __name__ == "__main__":
