92d56aacc40effd4a5396eb45cba7ed3f56216bf
msv28 <sleepyheroo@gmail.com>
2024-11-02 17:42:51 -0400

    Background changes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, url_for, request, jsonify
 import requests
+
 class Game:
-    def __init__(self, day) -> None:      
-        self.day = 30 if day > 30 else day
+    def __init__(self, day) -> None:
+        self.day = day
         self.expand = False
         self.citizens = 0
         self.standing = 0
@@ -32,8 +33,6 @@ class Game:
             return
         self.zones -= 1
         self.militaryZones += 1
-        self.standing += 1
-
 
 
         
