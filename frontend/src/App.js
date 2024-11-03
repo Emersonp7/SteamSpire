ab97ffc393b04b208c9ca81f0f99af8385eaec07
gorbe2002 <gorbe54@hotmail.com>
2024-11-02 22:53:10 -0400

    updated frontend, sorta :)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    };
-  }
-
   render() {
     return (
       <div>
-        
+        Hello
       </div>
     );
   }
 }
-
 export default App;
